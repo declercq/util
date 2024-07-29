ca5025c08433218d328db3b54b1881ea5f4de811
Andrew Declercq <andrew.declercq@gmail.com>
2024-07-29 10:26:47 -0500

    first pass at docker cache command

--- util/dcache/main.go
+++ util/dcache/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -13,14 +15,87 @@ var latestF *bool
 var uploadF *bool
 var sourceF *string
 
+func run(cmd string) (string, error) {
+	out, err := exec.Command(cmd).CombinedOutput()
+	sout := string(out)
+	if *debugF {
+		_, _ = fmt.Fprintf(os.Stderr, "cmd: %s\n", cmd)
+		_, _ = fmt.Fprintf(os.Stderr, "%s\n", sout)
+	}
+	if err != nil {
+		return "", fmt.Errorf("cmd (%s) failed: %w - %s", cmd, err, sout)
+	}
+	return sout, nil
+}
+
+func runf(format string, a ...any) (string, error) {
+	out, err := run(fmt.Sprintf(format, a...))
+	if err != nil {
+		return "", err
+	}
+	return out, err
+}
+
 func dcacheCmd(cmd *cobra.Command, args []string) error {
-	fmt.Printf("build: %t\n", *buildF)
-	fmt.Printf("debug: %t\n", *debugF)
-	fmt.Printf("latest: %t\n", *latestF)
-	fmt.Printf("upload: %t\n", *uploadF)
-	fmt.Printf("source: %s\n", *sourceF)
-	fmt.Printf("args: %v\n", args)
-	println("dcache called")
+	dirty := false
+	targetDir := args[0]
+	registry := args[1]
+	repository := args[2]
+	targetDirHash, err := runf("git log -n1 --pretty=format:\"%%h\" %s", targetDir)
+	if err != nil {
+		return err
+	}
+	wc, err := runf("git status --porcelain=v1 %s", targetDir)
+	if err != nil {
+		return err
+	}
+	if len(wc) > 0 {
+		*buildF = true
+		dirty = true
+	}
+	dockerPath := fmt.Sprintf("%s/%s:%s", registry, repository, targetDirHash)
+	date := time.Now().Format("20060102030405")
+	dockerPathLong := dockerPath + "." + date
+	dockerPathLatest := fmt.Sprintf("%s/%s:%s", registry, repository, "latest")
+	if !dirty {
+		// best effort to pull target image
+		_, _ = runf("docker pull %s", dockerPath)
+		wc, err = runf("docker image ls -q %s", dockerPath)
+		if err != nil {
+			return err
+		}
+		if len(wc) == 0 {
+			*buildF = true
+		}
+	}
+	if *buildF {
+		_, err := runf("docker build -f %s --tag %s %s", *sourceF, dockerPath, targetDir)
+		if err != nil {
+			return err
+		}
+		if !dirty {
+			_, err = runf("docker tag %s %s", dockerPath, dockerPathLong)
+			if err != nil {
+				return err
+			}
+			if *uploadF {
+				_, err = runf("docker push %s", dockerPath)
+				if err != nil {
+					return err
+				}
+				_, err = runf("docker push %s", dockerPathLong)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	if *latestF {
+		_, err = runf("docker tag %s %s", dockerPath, dockerPathLatest)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
